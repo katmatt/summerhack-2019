25a0bdfbff009640c4a36e9e3882255b8ce0a7d5
abeniasaad <achraf.beniasaad@commercetools.de>
2019-09-19 10:12:03 +0200

    module update

--- app/src/main/java/com/spaetimc/di/AppModule.kt
+++ app/src/main/java/com/spaetimc/di/AppModule.kt
@@ -1,13 +1,68 @@
 package com.spaetimc.di
 
+import com.commercetools.client.ApiRoot
 import dagger.Module
 import dagger.Provides
 import io.reactivex.disposables.CompositeDisposable
+import io.vrap.rmf.base.client.VrapHttpClient
+import io.vrap.rmf.base.client.middlewares.HttpMiddleware
+import io.vrap.rmf.base.client.oauth2.ClientCredentialsTokenSupplier
+import io.vrap.rmf.impl.okhttp.VrapOkhttpClient
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 internal class AppModule {
 
     @Provides
     fun provideCompositeDisposables() = CompositeDisposable()
+
+    @Provides
+    @Singleton
+    fun getHttpClient(): VrapHttpClient = VrapOkhttpClient()
+
+    @Provides
+    @Singleton
+    @Named("client_id")
+    fun getClientId(): String = ""
+
+    @Provides
+    @Singleton
+    @Named("client_secret")
+    fun getClientSecret(): String = ""
+
+    @Provides
+    @Singleton
+    @Named("project")
+    fun getProject(): String = ""
+
+    @Provides
+    @Singleton
+    @Named("scopes")
+    fun getScopes(
+        @Named("project_name") project_name:String
+    ): String = "manage_project:$project_name"
+
+    @Provides
+    @Singleton
+    fun getApiRoot(
+        vrapHttpClient: VrapHttpClient,
+        @Named("client_id") client_id: String,
+        @Named("client_secret") client_secret: String,
+        @Named("scopes") scopes: String
+    ): ApiRoot {
+        val httpMiddleware = HttpMiddleware(
+            "https://api.sphere.io",
+            vrapHttpClient,
+            ClientCredentialsTokenSupplier(
+                client_id,
+                client_secret,
+                scopes,
+                "https://auth.sphere.io/oauth/token",
+                vrapHttpClient
+            )
+        )
+        return ApiRoot.fromMiddlewares(httpMiddleware)
+    }
 
 }
