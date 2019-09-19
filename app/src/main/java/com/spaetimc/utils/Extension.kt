52784a65accd44fafea97d20ce37cd582ab38368
Mathias Oertel <e0e9cf7e@opayq.com>
2019-09-19 21:09:10 +0200

    - pixel pushing

---
+++ app/src/main/java/com/spaetimc/utils/Extension.kt
@@ -0,0 +1,44 @@
+package com.spaetimc.utils
+
+import com.commercetools.client.ByProjectKeyRequestBuilder
+import com.commercetools.models.cart.Cart
+import com.commercetools.models.cart.LineItemDraft
+import com.commercetools.models.cart.LineItemDraftImpl
+import com.commercetools.models.order.OrderFromCartDraft
+import com.commercetools.models.order.OrderFromCartDraftImpl
+import com.commercetools.models.product.Product
+import com.spaetimc.presentation.scan.model.AppProduct
+
+typealias AppProject = ByProjectKeyRequestBuilder
+
+fun productToAppProduct(product: Product): AppProduct = AppProduct(
+    name = product.masterData.current.name.values()["de-DE"] ?: "NO name found",
+    description = product.masterData.current.description.values()["de-DE"] ?: "NO description found",
+    pictureUrl = product.masterData.current.masterVariant.images[0]?.url ?: "default_url",
+    priceInCent = product.masterData.current.masterVariant.prices[0].value.centAmount.toInt(),
+    barcodeValue = product.masterData.current.masterVariant.sku,
+    createdAt = System.currentTimeMillis()
+)
+
+fun Int.format(): String {
+    val eur: Int = this / 100
+    val cent: Int = this % 100
+    return String.format("%d,%02d", eur, cent)
+}
+
+fun Product.priceAsString(): String =
+    if (this.masterData.current.masterVariant.prices[0] == null) "Price not available"
+    else "${this.masterData.current.masterVariant.prices[0].value.centAmount / 100} ${this.masterData.current.masterVariant.prices[0].value.currencyCode}"
+
+fun AppProduct.toLineItemDraft(): LineItemDraft =
+    LineItemDraftImpl().also { lineItemDraft ->
+        lineItemDraft.sku = this.barcodeValue
+        lineItemDraft.quantity = this.amount.toLong()
+    }
+
+fun Cart.toOrderDraft(): OrderFromCartDraft =
+    OrderFromCartDraftImpl().also { orderDraft ->
+        orderDraft.id = this.id
+        orderDraft.version = this.version
+        orderDraft.orderNumber = this.id
+    }
